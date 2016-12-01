3a207c1ef1d485a001dcbbbde5caa80520be919f
xdavidwu <david20000612@gmail.com>
2016-12-01 20:44:11 +0800

    AlertDialog example

---
+++ app/src/main/java/xdavidwu/demo/MainActivity.java
@@ -0,0 +1,27 @@
+package xdavidwu.demo;
+
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button showDialogButton = (Button) findViewById(R.id.button);
+        showDialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Hello")
+                        .setMessage("Hello World!")
+                        .show();
+            }
+        });
+    }
+}
